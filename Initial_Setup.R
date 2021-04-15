4dd8980e1d887dc69c612687b6cfc7deb88ffe75
Morgan Novikoff <morgannovikoff@pa-dhcp-10-120-227-85.gp-vpn.northwestern.private>
2021-04-15 16:48:42 -0500

    Initial Commit

--- Initial_Setup.R
+++ Initial_Setup.R
@@ -1,6 +1,7 @@
 # Load package(s)
 library(tidymodels)
 library(tidyverse)
+library(janitor)
 
 # Seed
 set.seed(3013)
@@ -15,3 +16,30 @@ wildfires_dat <- read_csv("data/wildfires.csv") %>%
   ) %>%
   select(-burned)
 
+
+#Split and fold data
+wildfire_split <- initial_split(wildfires_dat, prop = 0.8, strata = wlf) 
+wildfire_train <- training(wildfire_split) 
+wildfire_test <- testing(wildfire_split)
+
+wildfire_folds <- vfold_cv(wildfire_train, v = 5, repeats = 3, strata = wlf) 
+
+# Create recipe 
+
+wildfire_rec <- wildfire_train %>% 
+  recipe(wlf ~ . ) %>%
+  step_dummy(winddir, one_hot = TRUE) %>%
+  step_dummy(traffic, one_hot = TRUE) %>%
+  step_center(all_predictors()) %>%
+  step_scale(all_predictors())
+
+wildfire_rec %>% 
+  prep(wildfire_train) %>% 
+  bake(new_data = NULL) %>% 
+  view() 
+
+
+
+save(wildfires_dat, wildfire_train, wildfire_test, wildfire_split, wildfire_folds,
+     wildfire_rec,
+     file = "data/initial_setup.rda")
